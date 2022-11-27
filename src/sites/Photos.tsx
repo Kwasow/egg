23c914733f93b30ab97dc0b339d45a4cef1d00d2
Karol Wąsowski <wasowski02@protonmail.com>
2022-11-27 20:06:59 +0100

    Added file structure

---
+++ src/sites/Photos.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Photos() {
+
+}
+
+export default Photos
---
+++ src/sites/SpeakersAndOrganisers.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SpeakersAndOrganisers() {
+
+}
+
+export default SpeakersAndOrganisers

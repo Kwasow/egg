fa9decf58b25a6b5af9f133651bb98145f82c69d
Karol Wąsowski <wasowski02@protonmail.com>
2022-11-27 21:02:53 +0100

    Added MUI theme and example AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import './App.css';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 function App() {
-  
+  return <view>
+    <AppBar className='navigation-bar' position='static'>
+      <Toolbar variant='dense'>
+        <Typography variant='h6' color='inherit' component='div'>
+          Photos
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </view>
 }
 
 export default App
---
+++ src/utils/MUITheme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+export const eggTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#c53d63',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+})

8514a5c2d6bad3c0f274c4890234e77995454ef8
Karol Wąsowski <wasowski02@protonmail.com>
2022-12-05 11:13:50 +0100

    Added translation for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 function Footer() {
+  const {t} = useTranslation()
+
   return <footer>
     <p>
-      2022 © Studenckie koło naukowe położnictwa i ginekologii WUM
+      {t('footer.copyright')}
     </p>
   </footer>
 }

5385a7cdf9a031f343374443b0c8ca0db38eb3ba
Kwasow <wasowski02@protonmail.com>
2022-12-17 15:30:15 +0100

    Finish not found page (#11)

--- src/sites/NotFound.tsx
+++ src/sites/NotFound.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import './NotFound.css'
+
 function NotFound() {
-  return <p>Nie znaleziono strony</p>
+  return <>
+    <div className='container'>
+      <img className='ghost-icon' src='static/images/ghost.svg' />
+      <p className='error-boo'>Bu!</p>
+      <p className='error-text'>Obudziłeś ducha, który mieszka na tej pustej stronie</p>
+    </div>
+  </>
 }
 
 export default NotFound

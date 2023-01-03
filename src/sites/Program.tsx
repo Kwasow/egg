0c8b28c90d1e61d7406aa9da414f8096d1671f10
Karol Wąsowski <wasowski02@protonmail.com>
2023-01-03 09:38:47 +0100

    Added translations

--- src/sites/Program.tsx
+++ src/sites/Program.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import './Program.css'
 
 function Program() {
+  const {t} = useTranslation()
+
   const programImageURL = process.env.PUBLIC_URL + '/static/images/program.png'
 
   return <>
     <div className='gray-divider'>
       <div className='left-container'>
         <div>
-          <p className='text-primary text-primary-top'>Termin</p>
-          <p className='text-secondary'>14-16 kwietnia</p>
-          <p className='text-primary'>Adres</p>
+          <p className='text-primary text-primary-top'>
+            {t('program.Date.Label')}</p>
+          <p className='text-secondary'>{t('program.Date.Value')}</p>
+          <p className='text-primary'>{t('program.Venue.Label')}</p>
           <p className='text-secondary'>
-            Collegium Anatomicum<br/>ul. Tytusa Chałubińskiego 5, Warszawa</p>
+            {t('program.Venue.Value.Line1')}<br/>
+            {t('program.Venue.Value.Line2')}
+          </p>
         </div>
       </div>
       <img style={{ height: '400px' }} src={programImageURL}/>

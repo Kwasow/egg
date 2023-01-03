ea5d3baed201cdfc517f5ba665741b98af5cdb27
Karol Wąsowski <wasowski02@protonmail.com>
2023-01-03 09:30:28 +0100

    Added top part of program

--- src/sites/Program.tsx
+++ src/sites/Program.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import './Program.css'
+
 function Program() {
-  return <p>Program</p>
+  const programImageURL = process.env.PUBLIC_URL + '/static/images/program.png'
+
+  return <>
+    <div className='gray-divider'>
+      <div className='left-container'>
+        <div>
+          <p className='text-primary text-primary-top'>Termin</p>
+          <p className='text-secondary'>14-16 kwietnia</p>
+          <p className='text-primary'>Adres</p>
+          <p className='text-secondary'>
+            Collegium Anatomicum<br/>ul. Tytusa Chałubińskiego 5, Warszawa</p>
+        </div>
+      </div>
+      <img style={{ height: '400px' }} src={programImageURL}/>
+    </div>
+  </>
 }
 
 export default Program

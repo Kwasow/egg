ffbc355d99b2da75911a3846cc1b6da7412c821a
Karol Wąsowski <wasowski02@protonmail.com>
2023-01-11 19:38:50 +0100

    Moved slideshow to separate file

--- src/sites/HomePage.tsx
+++ src/sites/HomePage.tsx
@@ -1,69 +1,12 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import { Countdown } from '../components/Countdown'
+import { Slideshow } from '../components/Slideshow'
 import './HomePage.css'
-
-const colors = ['#0088FE', '#00C49F', '#FFBB28']
-const delay = 4000
-
-// TODO: Move this to a different file
-function Slideshow() {
-  const [index, setIndex] = useState(0)
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
-
-  function resetTimeout() {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current)
-    }
-  }
-
-  React.useEffect(() => {
-    resetTimeout()
-    timeoutRef.current = setTimeout(
-      () =>
-        setIndex((prevIndex) =>
-          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
-        ),
-      delay
-    )
-
-    return () => {
-      resetTimeout()
-    }
-  }, [index])
-
-  return (
-    <div className="slideshow">
-      <div
-        className="slideshowSlider"
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-      >
-        {colors.map((backgroundColor, index) => (
-          <div
-            className="slide"
-            key={index}
-            style={{ backgroundColor }}
-          ></div>
-        ))}
-      </div>
-
-      <div className='slideshowDots'>
-        {colors.map((_, idx) => (
-          <div
-            key={idx}
-            className={`slideshowDot${index === idx ? ' active' : ''}`}
-            onClick={() => {
-              setIndex(idx)
-            }}
-          ></div>
-        ))}
-      </div>
-    </div>
-  )
-}
 
 function HomePage() {
   return <>
     <Slideshow />
+    {/* TODO: Conference date */}
     <Countdown date={new Date('2023-04-15T18:00:00')}/>
   </>
 }

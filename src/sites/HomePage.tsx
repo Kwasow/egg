1ab1eb2bc82eedfb9ef1176520e3f4a77bded816
Karol Wąsowski <wasowski02@protonmail.com>
2023-01-11 18:48:00 +0100

    Added dummy slider

--- src/sites/HomePage.tsx
+++ src/sites/HomePage.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import './HomePage.css'
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28']
+const delay = 4000
+
+function Slideshow() {
+  const [index, setIndex] = useState(0)
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout()
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    )
+
+    return () => {
+      resetTimeout()
+    }
+  }, [index])
+
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {colors.map((backgroundColor, index) => (
+          <div
+            className="slide"
+            key={index}
+            style={{ backgroundColor }}
+          ></div>
+        ))}
+      </div>
+
+      <div className='slideshowDots'>
+        {colors.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? ' active' : ''}`}
+            onClick={() => {
+              setIndex(idx)
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 function HomePage() {
-  return <p>Aktualności</p>
+  return <>
+    <Slideshow></Slideshow>
+  </>
 }
 
 export default HomePage

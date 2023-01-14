b555af4a9c20f79e0ee34e1e06f5e0d0a8cc04ce
Karol Wąsowski <wasowski02@protonmail.com>
2023-01-14 23:27:15 +0100

    stash work

--- src/sites/HomePage.tsx
+++ src/sites/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Countdown } from '../components/Countdown'
 import { Slideshow, Slide } from '../components/Slideshow'
 import './HomePage.css'
@@ -30,11 +30,88 @@ const slides: Slide[] = [
   }
 ]
 
+interface News {
+  title_pl: string;
+  title_en: string;
+  text_pl: string;
+  text_en: string;
+  date: string;
+}
+
+interface NewsJSON {
+  registration: News;
+  other: News[];
+}
+
+function SponsorImage(props: {
+  image: string,
+  title?: string
+  imageAlt?: string,
+}) {
+  const {image, imageAlt, title} = props
+  const alt = imageAlt === undefined ? title : ''
+
+  return <div className='single-sponsor-container'>
+    <img src={image} alt={alt} className='sponsor-logo'/>
+    {title ? <p className='sponsor-subtext'>{title}</p> : <></>}
+  </div>
+}
+
+function Sponsors() {
+  return <>
+    <p className='sponsors-header'>Partnerzy</p>
+    <div className='sponsors-container'>
+      <SponsorImage 
+        image={process.env.PUBLIC_URL + '/static/sponsors/wum.png'}
+        title='Rektor Warszawskiego Uniwersytetu Medycznego'/>
+    </div>
+    <p className='sponsors-header'>Sponsorzy</p>
+    <div className='sponsors-container'>
+
+    </div>
+  </>
+}
+
+function News() {
+  const newsURL = 'static/news/news.json'
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [news, setNews] = useState<NewsJSON>()
+  const [loaded, setLoaded] = useState(0)
+
+  fetch(newsURL)
+    .then((res) => res.json())
+    .then((json: NewsJSON) => {
+      // 0 - not loaded
+      // 1 - loaded
+      // 2 - error
+      setNews(json)
+      setLoaded(1)
+    })
+    .catch((err) => {
+      console.error(err)
+      setLoaded(2)
+    })
+
+  if (loaded == 1) {
+    return <div className='news-container'>
+      <div className='news-registration'>
+
+      </div>
+    </div>
+  } else if (loaded == 2) {
+    return <div className='news-container'></div>
+  } else {
+    return <div className='news-container'></div>
+  }
+}
+
 function HomePage() {
   return <>
     <Slideshow slides={slides}/>
     {/* TODO: Conference date */}
     <Countdown date={new Date('2023-04-15T18:00:00')}/>
+    <News/>
+    <Sponsors/>
   </>
 }
 

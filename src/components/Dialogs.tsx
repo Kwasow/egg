601a4535fb60efbf669d855297b1bc5a16be62ab
Karol Wąsowski <wasowski02@protonmail.com>
2023-02-18 19:53:24 +0100

    Added transfer details to registration dialog

--- src/components/Dialogs.tsx
+++ src/components/Dialogs.tsx
@@ -36,15 +36,72 @@ export function RegisterNotAvailableDialog(props: {
   )
 }
 
+const transferDetails_pl = [
+  ['Dane do przelewu:'],
+  ['Adresat:', 'Warszawski Uniwersytet Medyczny'],
+  ['Nr konta:', '70 1160 2202 0000 0003 3828 9663'],
+  [
+    'Tytuł przelewu:',
+    '[Imię i Nazwisko], opłata konferencyjna, 4WG Sekrety Onkologii',
+  ],
+  ['Kwota:', '30zł'],
+]
+const transferDetails_en = [
+  ['Transfer details:'],
+  ['Recepient:', 'Warszawski Uniwersytet Medyczny'],
+  ['Account number:', '70 1160 2202 0000 0003 3828 9663'],
+  [
+    'Transfer title:',
+    '[Name and surname], opłata konferencyjna, 4WG Sekrety Onkologii',
+  ],
+  ['Value:', '30zł'],
+]
+
 export function RegistrationDialog(props: {
   open: boolean
   onClose: () => void
 }) {
   const { open, onClose } = props
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
 
   function openLink(link: string) {
     window.open(link)
+  }
+
+  function TransferDetails() {
+    if (i18n.language === 'pl') {
+      return (
+        <>
+          {transferDetails_pl.map((value, index) => {
+            if (index === 0) {
+              return <h3 key={index}>{value[0]}</h3>
+            } else {
+              return (
+                <div key={index}>
+                  <b>{value[0]}</b> {value[1]}
+                </div>
+              )
+            }
+          })}
+        </>
+      )
+    } else {
+      return (
+        <>
+          {transferDetails_en.map((value, index) => {
+            if (index === 0) {
+              return <h3 key={index}>{value[0]}</h3>
+            } else {
+              return (
+                <div key={index}>
+                  <b>{value[0]}</b> {value[1]}
+                </div>
+              )
+            }
+          })}
+        </>
+      )
+    }
   }
 
   return (
@@ -77,6 +134,7 @@ export function RegistrationDialog(props: {
             </td>
           </tr>
         </table>
+        <TransferDetails />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>OK</Button>

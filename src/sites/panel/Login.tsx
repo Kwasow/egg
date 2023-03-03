bb23b6e6d5734cc3fdd6934acdf6c2ae66fc3279
Karol Wąsowski <wasowski02@protonmail.com>
2023-03-03 13:22:48 +0100

    Design for login page

--- src/sites/panel/Login.tsx
+++ src/sites/panel/Login.tsx
@@ -1,11 +1,23 @@
+import { Button, TextField } from '@mui/material'
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuthentication } from '../../utils/useAuthentication'
+
+import './Login.css'
 
 export default function LoginPage() {
   if (useAuthentication().token) {
     return <Navigate replace to={'/admin'} />
   }
 
-  return <p>LoginPage</p>
+  return (
+    <div className='login-wrapper'>
+      <h1 className='title'>Logowanie do panelu administracyjnego</h1>
+      <p className='subtitle'>Weekend z Ginekologią</p>
+
+      <TextField className='input-field' label='Login' type={'text'} />
+      <TextField className='input-field' label='Hasło' type={'password'} />
+      <Button>Zaloguj</Button>
+    </div>
+  )
 }

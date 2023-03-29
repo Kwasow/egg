3ed862f563cc312fa4c0900ba693a6bb3ca55f6f
Karol Wąsowski <wasowski02@protonmail.com>
2023-03-29 17:59:19 +0200

    Sunshine v1

--- src/sites/Sunshine.tsx
+++ src/sites/Sunshine.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import './Sunshine.css'
 
 function Sunshine() {
-  return <div></div>
+  return (
+    <div className='main-wrapper'>
+      <p className='text-primary'>13/52</p>
+      <p className='text-secondary'>Niedługo…</p>
+    </div>
+  )
 }
 
 export default Sunshine

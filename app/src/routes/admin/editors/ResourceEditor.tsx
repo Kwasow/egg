90269a70d9c63c16caeb286e7589289400057378
Karol Wąsowski <wasowski02@protonmail.com>
2023-12-03 10:31:22 +0100

    Some work on resource editor

--- app/src/routes/admin/editors/ResourceEditor.tsx
+++ app/src/routes/admin/editors/ResourceEditor.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 import React from 'react'
 import { useAppDispatch } from '../../../utils/redux/hooks'
 import { useNavigate } from 'react-router-dom'
@@ -20,8 +20,33 @@ export default function ResourceEditor() {
       >
         Panel główny
       </Button>
-      <h1>Dodaj zasób</h1>
+
+      <AddResource />
       <h1>Edytuj zasoby</h1>
     </LoginProtected>
+  )
+}
+
+function AddResource() {
+  return (
+    <div>
+      <h1>Dodaj zasób</h1>
+      <form
+        encType='multipart/form-data'
+        action='https://webhook.site/32a503e0-0bce-45d8-a146-8611f0b7fc71'
+        method='post'
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <TextField variant='outlined' label='Nazwa' name='name' />
+
+        <label>Wybierz plik</label>
+        <input type='file' name='file' />
+
+        <Button type='submit'>Prześlij</Button>
+      </form>
+    </div>
   )
 }
